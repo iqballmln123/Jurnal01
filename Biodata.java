e765dc08b0fac1dddcf89dd361b10c1917c9f05f
iqballmln123 <iqbal12312366@gmail.com>
2023-09-27 16:12:09 +0700

    Jurnal-01

---
+++ Biodata.java
@@ -0,0 +1,7 @@
+public class Biodata {
+    public static void main(String[] args) {
+        System.out.println("NAMA: Mochamad Iqbal Maulana");
+        System.out.println("NIM: 607062300143");
+        System.out.println("Kelas: D3IF-47-01");
+    }
+}
